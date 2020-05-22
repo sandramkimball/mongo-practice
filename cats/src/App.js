e02c77d3dd9ba515b0d93e6d22e289086b36cf2d
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-05-22 14:29:54 -0700

    added cat form

--- cats/src/App.js
+++ cats/src/App.js
@@ -4,13 +4,57 @@ import './App.css'
 
 function App() {
     const [cats, setCats] = useState([])
+    const [newCat, setNewCat] = useState({
+        name: '',
+        hobbies: '',
+        url: ''
+    })
+
     useEffect(()=>{
         axios.get('http://localhost:5050/posts')
         .then(res=> setCats(res.data.catData))
         .catch(err=> console.log(err))
     })
+
+    const handleChange = e => {
+        e.preventDefault()
+        setNewCat({ [e.target.value]: value })
+    }
+
+    const handleCSubmit = e => {
+        e.preventDefault()
+        console.log('Meow Added')
+        // axios.put('http://localhost:5050/posts')
+        // .then(res=> setCats(res.data.catData))
+        // .catch(err=> console.log(err))
+    }
+
     return (
         <div className='App'>
+            <div>
+                <form>
+                    <p>Add a New Member to the Cat Family</p>
+                    <input
+                        onChange={handleChange}
+                        type='text'
+                        value='name'
+                        paceholder='Name'
+                    />
+                    <input
+                        onChange={handleChange}
+                        type='text'
+                        value='hobbies'
+                        palceholder='Hobbies'
+                    />
+                    <input
+                        onChange={handleChange}
+                        type='text'
+                        value='url'
+                        placeholder='Paste Image Url'
+                    />
+                    <button onSubmit={handleSubmit}>Submit</button>
+                </form>
+            </div>
             <div className='container'>
                 {cats.map(cat=> (
                     <div className='cat-card'>
